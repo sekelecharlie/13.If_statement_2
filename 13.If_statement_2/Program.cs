0d38dd3cfd27143b2de42d601bd32c2d22fa7c2f
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-13 08:34:02 +0200

    Add project files.

---
+++ 13.If_statement_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.If_statement_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number 1 ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2 ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetMax(num1, num2));
+            Console.ReadLine(); 
+        }
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                Console.WriteLine("The max is ");
+                return num1;
+            }
+            else
+            {
+                Console.WriteLine("The max is ");
+                return num2;
+            }
+        }
+    }
+}
